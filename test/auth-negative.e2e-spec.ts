4d372cfe134eca421152d34aa40c19eb50b2cba7
Ivan Fedulov <ivan-fedulov@users.noreply.github.com>
2026-09-13 02:25:43 +0300

    test: remove unused createTestUser imports

--- test/auth-negative.e2e-spec.ts
+++ test/auth-negative.e2e-spec.ts
@@ -1,8 +1,7 @@
-﻿import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { createTestUser } from './helpers/create-test-user';
 
 
 
